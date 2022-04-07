146d82fffad9661d6a00dc2fb5a94628248f0729
Cezar Castro Rosa <cecez86@gmail.com>
2022-04-07 12:04:10 -0300

    Initial Commit

---
+++ alura-viagens-swiftui-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  alura-viagens-swiftui-2
+//
+//  Created by Cezar Castro Rosa on 07/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ alura-viagens-swiftui-2/alura_viagens_swiftui_2App.swift
@@ -0,0 +1,17 @@
+//
+//  alura_viagens_swiftui_2App.swift
+//  alura-viagens-swiftui-2
+//
+//  Created by Cezar Castro Rosa on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct alura_viagens_swiftui_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
